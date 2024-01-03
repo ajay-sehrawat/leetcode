18495a0e87d68c4d345998eae5dac13f11bd099c
ajay-sehrawat <ajaysehrawat2302@gmail.com>
2024-01-03 14:35:47 +0530

    Adding New Solution

--- test.cpp
+++ test.cpp
@@ -1,32 +1,12 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int sum = 0;
 
-// Custom comparator function to compare two strings
-bool compareStrings(const std::string& str1, const std::string& str2) {
-    return str1 < str2; // Change the comparison logic as needed
-}
-
-int main() {
-    // Create a 2D vector of strings
-    std::vector<std::vector<std::string>> string2DVector = {
-        {"banana", "apple", "orange"},
-        {"grape", "pear", "kiwi"},
-        {"cherry", "strawberry", "blueberry"}
-    };
-
-    // Sort each row in the 2D vector
-    for (auto& row : string2DVector) {
-        std::sort(row.begin(), row.end(), compareStrings);
+    for( int i=0; i<1000; i++ )
+    {
+        
     }
-
-    // Print the sorted 2D vector
-    for (const auto& row : string2DVector) {
-        for (const auto& str : row) {
-            std::cout << str << " ";
-        }
-        std::cout << std::endl;
-    }
-
-    return 0;
+    cout<<sum;
 }
