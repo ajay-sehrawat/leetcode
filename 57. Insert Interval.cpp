cc3310f1b95b964092a04d9984f724aa5d1580d6
ajay-sehrawat <ajaysehrawat2302@gmail.com>
2024-06-11 12:52:38 +0530

    Adding New Solution

---
+++ 57. Insert Interval.cpp
@@ -0,0 +1,25 @@
+//Same as Solution 56.Merge intervals
+class Solution {
+public:
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+        intervals.push_back(newInterval);
+        sort(intervals.begin(),intervals.end());
+        int n = intervals.size();
+        vector<vector<int>> ans;
+        ans.push_back(intervals[0]);
+        int temp = intervals[0][1];
+        for( int i=1; i<n; i++ )
+        {
+            if ( temp >= intervals[i][0] )
+            {
+                temp = max(temp,intervals[i][1]);
+                ans.back()[1] = temp;
+            }
+            else{
+                ans.push_back(intervals[i]);
+                temp = intervals[i][1];
+            }
+        }
+        return ans;
+    }
+};
